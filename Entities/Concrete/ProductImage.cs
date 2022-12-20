7f85c03d0b6a212e35a45da8af1817e18fe09ac6
Kerem Berk Bayraktar <keremberk53@gmail.com>
2022-12-20 12:36:24 +0300

    Layered Architecture

---
+++ Entities/Concrete/ProductImage.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Concrete
+{
+    public class ProductImage
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
